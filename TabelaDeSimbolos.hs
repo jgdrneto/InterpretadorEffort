da15ca494bf527e0d14e5d3944eea62c9cb95591
Neto Gameleira <jgdrneto@gmail.com>
2016-11-17 16:32:29 -0300

    Começando a construir as funções associadas a tabela de simbolos

--- TabelaDeSimbolos.hs
+++ TabelaDeSimbolos.hs
@@ -2,4 +2,48 @@ module TabelaDeSimbolos(
 
 )where
 
-import Tipos	
+import Tipos
+
+criarSimbolo:: Tipos.Nome -> [Valor] -> Tipo -> Escopo -> Simbolo
+criarSimbolo (nome,valor,tipo,escopo) = (nome,valor,tipo,escopo)
+
+inserirSimbolo:: TabelaDeSimbolos -> Simbolo -> TabelaDeSimbolos
+inserirSimbolo ([],s) = s:[]
+inserirSimbolo (tbs,()) = tbs 
+inserirSimbolo (tbs,simbolo) = if (procurarSimbolo tbs simbolo == ()) then tbs:simbolo
+							   else tbs	
+
+excluirSimbolo:: TabelaDeSimbolos -> Simbolo -> TabelaDeSimbolos
+excluirSimbolo([],simbolo) = []
+excluirSimbolo(tbs,()) = tbs
+excluirSimbolo(cbtbs:caldatbs,simbolo) = if(fst simbolo == fst cbtbs) then caldatbs
+										 else  cbtbs:excluirSimbolo caldatbs simbolo
+
+procurarSimbolo:: TabelaDeSimbolos -> Simbolo -> Simbolo
+procurarSimbolo([],()) = ()
+procurarSimbolo([], s) = ()
+procurarSimbolo(tbs,()) = ()
+procurarSimbolo(cbtbs:caldatbs,simbolo) = if(fst simbolo == fst cbtbs) then cbtbs
+										  else  cbtbs:procurarSimbolo caldatbs simbolo
+
+existeSimbolo:: TabelaDeSimbolos -> Simbolo -> Bool
+existeSimbolo([], s) = False
+existeSimbolo(tbs,()) = False
+existeSimbolo(cbtbs:caldatbs,simbolo) = if(fst simbolo == fst cbtbs) then True
+										else existeSimbolo caldatbs simbolo
+
+procurarSimboloPorNome:: TabelaDeSimbolos -> Nome -> Simbolo
+procurarSimboloPorNome([],"") = ()
+procurarSimboloPorNome(tbs,"") = ()
+procurarSimboloPorNome([],s) = ()
+procurarSimboloPorNome(cbtbs:caldatbs,simbolo) = if(fst simbolo == fst cbtbs) then cbtbs
+												 else procurarSimboloPorNome caldatbs simbolo
+
+procurarValorPorNome:: TabelaDeSimbolos -> Nome -> Valor 
+procurarValorPorNome([],nome) = []
+procurarValorPorNome(tbs,"") = []
+procurarValorPorNome(tbs, nome) = procurarSimboloPorNome(tbs,nome)
+
+
+procurarValorNoSimbolo:: Simbolo -> Nome -> Valor
+procurarValorNoSimbolo((),"") = NULL
--- Tipos.hs
+++ Tipos.hs
@@ -2,5 +2,27 @@ module Tipos(
 
 )where
 
---type Nome 
---type TabelaDeSimbolos = [()]	
+import Data.List
+import Data.Array
+import Data.Ix
+
+-- Definindo tipos primitivos na nossa linguagem 
+type Matriz = Array(Int,Int) Tipo
+type Polinomio = Array Int Numero
+type NULL = String
+
+--Todos os tipos primitivos da nossa linguagem
+data Tipo = Numero | Char | Matriz | Polinomio
+data Numero = Int | Float | Double
+
+--Definindo valores dos tipos
+data Valor = StringV String | IntV Int | FloatV Float | DoubleV Double | CharV Char | MatrizV Matriz Int Int Tipo | PolinomioV Polinomio Int Numero | NULLV NULL 
+
+--Construindo tabela de símbolos
+type Nome = String
+type Escopo = String
+type Simbolo = (Nome, Tipo, [Valor], Escopo)
+type TabelaDeSimbolos = [Simbolo]
+
+
+
