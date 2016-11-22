f7e3530ef63cc51a3e9df5c2d4d46d8fcd481c9a
Neto Gameleira <jgdrneto@gmail.com>
2016-11-22 18:17:44 -0300

    Mudado tabela de simbolos

--- TabelaDeSimbolos.hs
+++ TabelaDeSimbolos.hs
@@ -30,44 +30,50 @@ inserirVariavel:: TabelaDeSimbolos -> Simbolo -> TabelaDeSimbolos
 inserirVariavel [] s = s:[] 
 inserirVariavel tbs simbolo = tbs++(simbolo:[])
 
-buscarValor::TabelaDeSimbolos -> Nome -> Valor
-buscarValor [] _ = NULL
-buscarValor tbs "" = NULL
-buscarValor (cab:cald) n = if(n == nome cab) then valor cab  
-                           else buscarValor cald n
+buscarValor::TabelaDeSimbolos -> Nome  -> Escopo -> Valor
+buscarValor [] _ _ = NULL
+buscarValor tbs "" _ = NULL
+buscarValor tbs _ "" = NULL
+buscarValor (cab:cald) n e = if(n == nome cab && e == escopo cab) then valor cab  
+                             else buscarValor cald n e
 
-buscarTipo::TabelaDeSimbolos -> Nome -> Tipo
-buscarTipo [] _ = SEMTIPO
-buscarTipo tbs "" = SEMTIPO
-buscarTipo (cab:cald) n = if(n == nome cab) then tipo cab  
-                          else buscarTipo cald n
+buscarTipo::TabelaDeSimbolos -> Nome -> Escopo -> Tipo
+buscarTipo [] _ _ = SEMTIPO
+buscarTipo tbs "" _ = SEMTIPO
+buscarTipo tbs _ "" = SEMTIPO
+buscarTipo (cab:cald) n e = if(n == nome cab && e == escopo cab) then tipo cab  
+                          else buscarTipo cald n e
 
-buscarEscopo::TabelaDeSimbolos -> Nome -> Escopo
-buscarEscopo [] _ = "Nulo"
-buscarEscopo tbs "" = "Nulo"
-buscarEscopo (cab:cald) n = if(n == nome cab) then escopo cab  
-                            else buscarEscopo cald n
+--buscarEscopo::TabelaDeSimbolos -> Nome -> Escopo
+--buscarEscopo [] _ = "Nulo"
+--buscarEscopo tbs "" = "Nulo"
+--buscarEscopo (cab:cald) n = if(n == nome cab) then escopo cab  
+--                            else buscarEscopo cald n
 
-excluirPorNome::TabelaDeSimbolos -> Nome -> TabelaDeSimbolos
-excluirPorNome [] _ = []
-excluirPorNome tab "" = tab
-excluirPorNome (cab:cal) n = if (n == nome cab) then cal
-                              else cab:excluirPorNome cal n
+excluirPorNome::TabelaDeSimbolos -> Nome ->Escopo -> TabelaDeSimbolos
+excluirPorNome [] _ _ = []
+excluirPorNome tbs "" _ = tbs
+excluirPorNome tbs _ "" = tbs
+excluirPorNome (cab:cal) n e = if (n == nome cab && e == escopo cab) then cal
+                               else cab:excluirPorNome cal n e
 
-existeVariavel::TabelaDeSimbolos -> Nome -> Bool
-existeVariavel [] _ = False
-existeVariavel tab "" = False
-existeVariavel (cab:cal) n = if (n == nome cab) then True
-                             else existeVariavel cal n
+existeVariavel::TabelaDeSimbolos -> Nome -> Escopo -> Bool
+existeVariavel [] _ _ = False
+existeVariavel tbs "" _ = False
+existeVariavel tbs _ "" = False
+existeVariavel (cab:cal) n e = if (n == nome cab && e == escopo cab) then True
+                               else existeVariavel cal n e
 
-valoresCompativeis::TabelaDeSimbolos -> Nome -> Nome -> Bool
-valoresCompativeis [] _ _ = False
-valoresCompativeis tab "" _ = False
-valoresCompativeis tab _ "" = False
-valoresCompativeis tab n1 n2 = buscarTipo tab n1 == buscarTipo tab n2
+valoresCompativeis::TabelaDeSimbolos -> Nome -> Escopo -> Nome -> Escopo -> Bool
+valoresCompativeis [] _ _ _ _= False
+valoresCompativeis tbs "" _ _ _ = False
+valoresCompativeis tbs _ "" _ _ = False
+valoresCompativeis tbs _ _ "" _ = False
+valoresCompativeis tbs _ _ _ "" = False
+valoresCompativeis tbs n1 e1 n2 e2 = buscarTipo tbs n1 e1 == buscarTipo tbs n2 e2
 
 excluirPorEscopo::TabelaDeSimbolos -> Escopo -> TabelaDeSimbolos
 excluirPorEscopo [] _ = []
 excluirPorEscopo tab "" = tab
 excluirPorEscopo (cab:cal) e = if(e==escopo cab) then excluirPorEscopo cal e
-                            else cab:excluirPorEscopo cal e 
+                               else cab:excluirPorEscopo cal e 
